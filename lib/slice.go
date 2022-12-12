bf4fabd09c647e28f28fed59c145ccb85351318a
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2022-12-11 21:08:34 -0500

    Add intersect on slice and pepper in generics

--- lib/slice.go
+++ lib/slice.go
@@ -112,7 +112,7 @@ func Seq(min int, max int) []int {
 }
 
 // Index returns the first index of the target string t, or -1 if no match is found
-func Index(vs []string, t string) int {
+func Index[T comparable](vs []T, t T) int {
 	for i, v := range vs {
 		if v == t {
 			return i
@@ -122,12 +122,12 @@ func Index(vs []string, t string) int {
 }
 
 // Include returns true if the target string t is in the slice
-func Include(vs []string, t string) bool {
+func Include[T comparable](vs []T, t T) bool {
 	return Index(vs, t) >= 0
 }
 
 // Any returns true if one of the strings in the slice satisfies the predicate f
-func Any(vs []string, f func(string) bool) bool {
+func Any[T any](vs []T, f func(T) bool) bool {
 	for _, v := range vs {
 		if f(v) {
 			return true
@@ -137,7 +137,7 @@ func Any(vs []string, f func(string) bool) bool {
 }
 
 // All returns true if all of the strings in the slice satisfy the predicate f
-func All(vs []string, f func(string) bool) bool {
+func All[T any](vs []T, f func(T) bool) bool {
 	for _, v := range vs {
 		if !f(v) {
 			return false
@@ -147,8 +147,8 @@ func All(vs []string, f func(string) bool) bool {
 }
 
 // Filter returns a new slice containing all strings in the slice that satisfy the predicate f
-func Filter(vs []string, f func(string) bool) []string {
-	vsf := make([]string, 0)
+func Filter[T any](vs []T, f func(T) bool) []T {
+	vsf := make([]T, 0)
 	for _, v := range vs {
 		if f(v) {
 			vsf = append(vsf, v)
@@ -158,10 +158,16 @@ func Filter(vs []string, f func(string) bool) []string {
 }
 
 // Map returns a new slice containing the results of applying the function f to each string in the original slice
-func Map(vs []string, f func(string) string) []string {
-	vsm := make([]string, len(vs))
+func Map[T any](vs []T, f func(T) T) []T {
+	vsm := make([]T, len(vs))
 	for i, v := range vs {
 		vsm[i] = f(v)
 	}
 	return vsm
+}
+
+func Intersect[T comparable](vs1 []T, vs2 []T) []T {
+	return Filter(vs1, func(v T) bool {
+		return Include(vs2, v)
+	})
 }
